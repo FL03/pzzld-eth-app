d5a09e3da1a9932ac95efa9848d9cda9cfd42895
Joe McCain III <jo3mccain@icloud.com>
2022-12-16 22:50:23 +0000

    Update

--- wasm/tests/default.rs
+++ wasm/tests/default.rs
@@ -1,4 +1,6 @@
 #[cfg(test)]
+use wasm::greet;
+
 use wasm_bindgen_test::wasm_bindgen_test;
 
 #[wasm_bindgen_test]
@@ -6,4 +8,10 @@ fn lib_compiles() {
     let f = |i: usize| i + 1;
 
     assert_eq!(f(10), 11)
+}
+
+#[wasm_bindgen_test]
+fn greet_test() {
+    let a = greet("");
+    assert!(true);
 }
