5f61077dfbbd4488139b44dcbd9f687318baf44f
Joe McCain III <jo3mccain@icloud.com>
2023-01-03 19:07:27 +0000

    Update

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,14 +1,10 @@
-import adapter_cloudflare from '@sveltejs/adapter-node';
 import adapter from '@sveltejs/adapter-node';
 import adapter_vercel from '@sveltejs/adapter-vercel';
 import preprocess from "svelte-preprocess";
 
 const config = {
 	kit: {
-		...(process.env.MODE === "cloudflare") && {
-			adapter: adapter_cloudflare()
-		},
-		...(process.env.MODE !== "cloudflare") && {
+		...(process.env.MODE !== "vercel") && {
 			adapter: adapter()
 		},
 		...(process.env.MODE === "vercel") && {
