1b01903524764bc0cdecd408a1e8ed6e2664fb7b
Joe McCain III <jo3mccain@icloud.com>
2023-01-03 18:20:22 +0000

    Update

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,15 @@
+import adapter_cloudflare from '@sveltejs/adapter-node';
 import adapter from '@sveltejs/adapter-node';
 import preprocess from "svelte-preprocess";
 
 const config = {
 	kit: {
-		adapter: adapter()
+		...(process.env.MODE === "cloudflare") && {
+			adapter: adapter_cloudflare()
+		},
+		...(process.env.MODE !== "cloudflare") && {
+			adapter: adapter()
+		}
 	},
 	preprocess: [
 		preprocess({
