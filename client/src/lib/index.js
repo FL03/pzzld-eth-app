4e7175e9ab7f86bed70632f572c1cd7b8b7bb958
FL03 <jo3mccain@icloud.com>
2023-01-26 17:42:26 -0600

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- client/src/lib/index.js
+++ client/src/lib/index.js
@@ -2,13 +2,12 @@ import { Card, CardBody, CardHeader, CardFooter, ProfileCard } from './cards/ind
 import { Form } from './forms/index.js';
 import { List } from './lists/index.js';
 import { Navbar, Navlink, NavLogo, Sidebar, SidebarToggle, Searchbar, Toolbar } from './nav/index.js'
-import { Footer, Header, Section } from './sections/index.js';
-import { Table } from './tables/index.js';
+import { Footer, Section } from './sections/index.js';
 import { WalletToggle } from './wallet/index.js';
 
 export { CardBody, CardHeader, CardFooter, ProfileCard, Searchbar }
-export { Footer, Header, Section } 
-export { Card, Form, List, Table }
+export { Footer, Section } 
+export { Card, Form, List }
 export { Navbar, Navlink, NavLogo, Sidebar, SidebarToggle, Toolbar }
 
 export { WalletToggle }
--- client/src/lib/sections/index.js
+++ client/src/lib/sections/index.js
@@ -1,7 +1,5 @@
 import { Footer } from './footer/index.js';
-import { Header } from './header/index.js';
 
 import Section from './Section.svelte';
 
-export { Footer, Header }
-export { Section }
+export { Footer, Section }
